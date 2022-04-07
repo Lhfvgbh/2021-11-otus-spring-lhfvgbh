7ff91414cc8c06a68121ed6c02acaebfc4cf0b8c
Elizaveta Isaeva <will-lizok@mail.ru>
2022-04-07 09:38:02 +0300

    Homework 9 version 2.2
    Homework 9 version 2.2

--- homework_9/src/test/java/ru/otus/homework_9/controller/BookControllerTest.java
+++ homework_9/src/test/java/ru/otus/homework_9/controller/BookControllerTest.java
@@ -13,7 +13,7 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@DisplayName("Тестирование BookRestController")
+@DisplayName("Тестирование BookController")
 @WebMvcTest(BookController.class)
 public class BookControllerTest {
 
