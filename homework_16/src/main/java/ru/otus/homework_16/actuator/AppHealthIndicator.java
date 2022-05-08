320f4c8aecc3d0c10a276d80efe47af6bf24060d
Elizaveta Isaeva <will-lizok@mail.ru>
2022-05-09 00:36:40 +0300

    Homework-16 version 2.0
    Homework-16 version 2.0

--- homework_16/src/main/java/ru/otus/homework_16/actuator/AppHealthIndicator.java
+++ homework_16/src/main/java/ru/otus/homework_16/actuator/AppHealthIndicator.java
@@ -1,27 +1,32 @@
 package ru.otus.homework_16.actuator;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.actuate.health.Status;
+import org.springframework.stereotype.Component;
+import ru.otus.homework_16.service.ActuatorServiceImpl;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-@RequiredArgsConstructor
+@Component
 public class AppHealthIndicator implements HealthIndicator {
-    @Value("${spring.datasource.url}")
     private final String url;
+    private final ActuatorServiceImpl service;
+
+    public AppHealthIndicator(@Value("${spring.datasource.url}") String url,
+                              ActuatorServiceImpl service) {
+        this.url = url;
+        this.service = service;
+    }
 
     @Override
     public Health health() {
         try {
-            Connection connection = DriverManager.getConnection(url, "", "");
-            PreparedStatement ps = connection.prepareStatement("SELECT id FROM books limit 1");
-            ps.executeQuery();
+            service.checkDBState(url, "sa", "");
             return Health.up().status(Status.UP).withDetail("message", "DB is available").build();
         } catch (SQLException e) {
             e.printStackTrace();
